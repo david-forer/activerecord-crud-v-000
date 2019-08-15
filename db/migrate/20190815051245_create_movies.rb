74f03fffbd43bd0742f3aac65ca62edd231787cb
David Forer <david.j.forer@gmail.com>
2019-08-15 05:15:11 +0000

    Automatically backed up by Learn

--- db/migrate/20190815051245_create_movies.rb
+++ db/migrate/20190815051245_create_movies.rb
@@ -1,8 +1,8 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
     
-    create_table :dogs do |t|
-      t.string :name
+    create_table :movies do |t|
+      t.string :title
       t.string :breed
     end
     
